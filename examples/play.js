281160dbce6bf86220aa675e83228194e8a158dc
谭聪 <156638174@qq.com>
2023-08-23 22:59:52 +0800

    tmp

--- examples/play.js
+++ examples/play.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Element from 'main/index.js';
 // import App from './play/index.vue';
-import App from './play/scrollLocationTest.vue';
+// import App from './play/scrollLocationTest.vue';
+import App from './play/scrollBar.vue';
 import 'packages/theme-chalk/src/index.scss';
 
 Vue.use(Element);
---
+++ examples/play/scrollBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-select v-model="value" placeholder="请选择">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        options: new Array(20).fill(0).map((v, idx) => {
+        return {
+          value: idx,
+          label: `第${idx + 1}个`,
+        };
+      }),
+        value: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
